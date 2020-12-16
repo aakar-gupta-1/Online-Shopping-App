3296aeb8b040ec5179a64fca698d58229731af42
Aakar <bhoothnath552@gmail.com>
2020-12-16 12:12:30 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_shop/providers/orders.dart';
+import 'package:my_shop/screens/orders_screen.dart';
 import 'screens/cart_screen.dart';
 import 'package:provider/provider.dart';
 import 'screens/products_overview_screen.dart';
@@ -35,6 +36,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
         },
       ),
     );
